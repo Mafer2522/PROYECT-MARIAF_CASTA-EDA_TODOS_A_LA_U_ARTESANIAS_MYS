e3b98a147b9d5792a9fcc6baf6eaed57633eebe2
Mafer2522 <117791400+Mafer2522@users.noreply.github.com>
2023-06-12 14:51:37 -0500

    Add files via upload

---
+++ BACKEND_FRONTEND_PROYECTO-MYS-ARTESANIAS_MARIAF_CASTANEDA/Iniciosesion/php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root", "", "mysart_proyecto");
+
+/*Conprobación Conexión base de datos
+
+if($conexion){
+  echo  'Conectado exitosamente a la Base de Datos';
+}else{
+    echo 'No se ha podido conectar a la Base de Datos';
+}
+
+*/
+
+?>
